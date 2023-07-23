eaff778c4988a822f237226fc2dfaf829f4c2ad8
Christian Schwarz <me@cschwarz.com>
2023-07-23 13:51:32 +0000

    refactored, now less code per op, which his good

--- src/ops.rs
+++ src/ops.rs
@@ -1,1 +1,125 @@
 pub mod read;
+
+use std::{
+    os::fd::{AsRawFd, OwnedFd},
+    pin::Pin,
+};
+
+use futures::{Future, FutureExt};
+
+use crate::system::{ResourcesOwnedByKernel, SubmitSide, SubmitSideProvider};
+
+pub(crate) enum OpFut<L, P, O>
+where
+    L: Future<Output = P> + Unpin,
+    P: SubmitSideProvider,
+    O: OpTrait + Send + 'static,
+{
+    Undefined,
+    NeedLaunch {
+        system_launcher: L,
+        make_op: O,
+    },
+    NeedSlot {
+        slot_fut: crate::system::GetOpsSlotFut,
+        make_op: O,
+    },
+    Submitted(crate::system::InflightOpHandle<O>),
+    ReadyPolled,
+}
+
+pub(crate) trait OpTrait: ResourcesOwnedByKernel + Sized + Send + 'static {
+    fn make_sqe(&mut self) -> io_uring::squeue::Entry;
+    fn into_fut<L, P>(self, launcher: L) -> OpFut<L, P, Self>
+    where
+        L: Future<Output = P> + Unpin,
+        P: SubmitSideProvider,
+    {
+        OpFut::NeedLaunch {
+            system_launcher: launcher,
+            make_op: self,
+        }
+    }
+}
+
+pub(crate) fn op<L, P, O, R>(system_launcher: L, make_op: O) -> OpFut<L, P, O>
+where
+    L: Future<Output = P> + Unpin,
+    P: SubmitSideProvider,
+    O: OpTrait + Send + 'static,
+{
+    OpFut::NeedLaunch {
+        system_launcher,
+        make_op,
+    }
+}
+
+impl<L, P, O> std::future::Future for OpFut<L, P, O>
+where
+    L: Future<Output = P> + Unpin,
+    P: SubmitSideProvider,
+    O: OpTrait + Send + 'static + Unpin,
+{
+    type Output = O::OpResult;
+
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut myself = self.as_mut();
+        loop {
+            let cur = std::mem::replace(&mut *myself, OpFut::Undefined);
+            match cur {
+                OpFut::Undefined => todo!(),
+                OpFut::ReadyPolled => unreachable!(),
+                OpFut::NeedLaunch {
+                    mut system_launcher,
+                    make_op,
+                } => match system_launcher.poll_unpin(cx) {
+                    std::task::Poll::Ready(submit_provider) => {
+                        let slot_fut = submit_provider.with_submit_side(|submit_side| {
+                            let SubmitSide(inner) = submit_side;
+                            let mut submit_side_guard = inner.lock().unwrap();
+                            let submit_side_open = submit_side_guard.must_open();
+                            submit_side_open.get_ops_slot()
+                        });
+                        *myself = OpFut::NeedSlot { slot_fut, make_op };
+                        continue;
+                    }
+                    std::task::Poll::Pending => {
+                        *myself = OpFut::NeedLaunch {
+                            system_launcher,
+                            make_op,
+                        };
+                        return std::task::Poll::Pending;
+                    }
+                },
+                OpFut::NeedSlot {
+                    mut slot_fut,
+                    make_op,
+                } => match slot_fut.poll_unpin(cx) {
+                    std::task::Poll::Pending => {
+                        *myself = OpFut::NeedSlot { slot_fut, make_op };
+                        return std::task::Poll::Pending;
+                    }
+                    std::task::Poll::Ready(slot) => {
+                        let submit_fut = slot.submit(make_op, |op| op.make_sqe());
+                        *myself = OpFut::Submitted(submit_fut);
+                        continue;
+                    }
+                },
+                OpFut::Submitted(mut submit_fut) => match submit_fut.poll_unpin(cx) {
+                    std::task::Poll::Ready(output) => {
+                        let output: O::OpResult = output;
+                        *myself = OpFut::ReadyPolled;
+                        return std::task::Poll::Ready(output);
+                    }
+                    std::task::Poll::Pending => {
+                        *myself = OpFut::Submitted(submit_fut);
+                        return std::task::Poll::Pending;
+                    }
+                },
+            }
+        }
+    }
+}
--- src/ops/read.rs
+++ src/ops/read.rs
@@ -1,153 +1,48 @@
 use std::os::fd::{AsRawFd, OwnedFd};
 
-use futures::{Future, FutureExt};
+use crate::{system::ResourcesOwnedByKernel, SubmitSideProvider};
 
-use crate::system::{ResourcesOwnedByKernel, SubmitSide, SubmitSideProvider};
+use super::OpTrait;
 
-enum ReadFut<L, P, B>
-where
-    P: SubmitSideProvider,
-    L: Future<Output = P>,
-    B: tokio_uring::buf::IoBufMut + Send,
-{
-    Undefined,
-    NeedLaunch {
-        system_launcher: L,
-        file: OwnedFd,
-        offset: u64,
-        buf: B,
-    },
-    WaitSlot {
-        slot_fut: crate::system::GetOpsSlotFut,
-        file: OwnedFd,
-        offset: u64,
-        buf: B,
-    },
-    Submitted(crate::system::InflightOpHandle<Read<B>>),
-    ReadyPolled,
-}
-
-pub fn read<L, P, B>(
+pub async fn read<'a, L, P, B>(
     system_launcher: L,
     file: OwnedFd,
     offset: u64,
     buf: B,
-) -> impl Future<Output = (OwnedFd, B, std::io::Result<usize>)>
+) -> (OwnedFd, B, std::io::Result<usize>)
 where
+    L: std::future::Future<Output = P> + Unpin,
     P: SubmitSideProvider,
-    L: Future<Output = P> + Unpin,
-    B: tokio_uring::buf::IoBufMut + Send,
+    B: tokio_uring::buf::IoBufMut + Send + 'a,
 {
-    ReadFut::<L, P, B>::NeedLaunch {
-        system_launcher,
-        file: file,
-        offset: offset,
-        buf: buf,
-    }
+    ReadOp { file, offset, buf }.into_fut(system_launcher).await
 }
 
-impl<L, P, B> std::future::Future for ReadFut<L, P, B>
-where
-    P: SubmitSideProvider,
-    L: Future<Output = P> + Unpin,
-    B: tokio_uring::buf::IoBufMut + Send,
-{
-    type Output = (OwnedFd, B, std::io::Result<usize>);
-
-    fn poll(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Self::Output> {
-        let mut myself = self.as_mut();
-        loop {
-            let cur = std::mem::replace(&mut *myself, ReadFut::Undefined);
-            match cur {
-                ReadFut::Undefined => todo!(),
-                ReadFut::ReadyPolled => unreachable!(),
-                ReadFut::NeedLaunch {
-                    mut system_launcher,
-                    file,
-                    offset,
-                    buf,
-                } => match system_launcher.poll_unpin(cx) {
-                    std::task::Poll::Ready(submit_provider) => {
-                        let slot_fut = submit_provider.with_submit_side(|submit_side| {
-                            let SubmitSide(inner) = submit_side;
-                            let mut submit_side_guard = inner.lock().unwrap();
-                            let submit_side_open = submit_side_guard.must_open();
-                            submit_side_open.get_ops_slot()
-                        });
-                        *myself = ReadFut::WaitSlot {
-                            slot_fut,
-                            file,
-                            offset,
-                            buf,
-                        };
-                        continue;
-                    }
-                    std::task::Poll::Pending => {
-                        *myself = ReadFut::NeedLaunch {
-                            system_launcher,
-                            file,
-                            offset,
-                            buf,
-                        };
-                        return std::task::Poll::Pending;
-                    }
-                },
-                ReadFut::WaitSlot {
-                    mut slot_fut,
-                    file,
-                    offset,
-                    buf,
-                } => match slot_fut.poll_unpin(cx) {
-                    std::task::Poll::Ready(slot) => {
-                        let submit_fut = slot.submit(Read { file, buf }, |preadv| {
-                            io_uring::opcode::Read::new(
-                                io_uring::types::Fd(preadv.file.as_raw_fd()),
-                                preadv.buf.stable_mut_ptr(),
-                                preadv.buf.bytes_total() as _,
-                            )
-                            .offset(offset)
-                            .build()
-                        });
-                        *myself = ReadFut::Submitted(submit_fut);
-                        continue;
-                    }
-                    std::task::Poll::Pending => {
-                        *myself = ReadFut::WaitSlot {
-                            slot_fut,
-                            file,
-                            offset,
-                            buf,
-                        };
-                        return std::task::Poll::Pending;
-                    }
-                },
-                ReadFut::Submitted(mut submit_fut) => match submit_fut.poll_unpin(cx) {
-                    std::task::Poll::Ready((file, buf, res)) => {
-                        *myself = ReadFut::ReadyPolled;
-                        return std::task::Poll::Ready((file, buf, res));
-                    }
-                    std::task::Poll::Pending => {
-                        *myself = ReadFut::Submitted(submit_fut);
-                        return std::task::Poll::Pending;
-                    }
-                },
-            }
-        }
-    }
-}
-
-struct Read<B>
+struct ReadOp<B>
 where
     B: tokio_uring::buf::IoBufMut + Send,
 {
     file: OwnedFd,
+    offset: u64,
     buf: B,
 }
 
-impl<B> ResourcesOwnedByKernel for Read<B>
+impl<B> OpTrait for ReadOp<B>
+where
+    B: tokio_uring::buf::IoBufMut + Send,
+{
+    fn make_sqe(&mut self) -> io_uring::squeue::Entry {
+        io_uring::opcode::Read::new(
+            io_uring::types::Fd(self.file.as_raw_fd()),
+            self.buf.stable_mut_ptr(),
+            self.buf.bytes_total() as _,
+        )
+        .offset(self.offset)
+        .build()
+    }
+}
+
+impl<B> ResourcesOwnedByKernel for ReadOp<B>
 where
     B: tokio_uring::buf::IoBufMut + Send,
 {
