7f80b73784294d70320a75d24c0bd0460698b588
Joonas Koivunen <joonas@neon.tech>
2023-08-02 17:41:14 +0300

    chore: tame the boxed type

--- tokio-epoll-uring/src/ops/mod.rs
+++ tokio-epoll-uring/src/ops/mod.rs
@@ -10,6 +10,20 @@ pub use crate::system::submission::op_fut::Op;
 pub mod nop;
 pub mod read;
 
+#[allow(type_alias_bounds)]
+type BoxedOpFuture<O: Op> = Pin<
+    Box<
+        dyn std::future::Future<
+                Output = (
+                    <O as Op>::Resources,
+                    Result<<O as Op>::Success, crate::SystemError<<O as Op>::Error>>,
+                ),
+            >
+            + 'static
+            + Send,
+    >,
+>;
+
 /// The io_uring operations supported by this crate.
 ///
 /// Use directly on any of the "Implementors" (see below)
@@ -17,20 +31,8 @@ pub mod read;
 // FIXME: why does this exist?
 pub trait Ops {
     /// See <https://man.archlinux.org/man/extra/liburing/io_uring_prep_nop.3.en>.
-    fn nop(
-        &self,
-    ) -> Pin<
-        Box<
-            dyn std::future::Future<
-                    Output = (
-                        (),
-                        Result<(), crate::system::submission::op_fut::Error<std::io::Error>>,
-                    ),
-                >
-                + 'static
-                + Send,
-        >,
-    >;
+    fn nop(&self) -> BoxedOpFuture<crate::ops::nop::Nop>;
+
     /// Read up to `buf.bytes_total()` bytes from the given `file` at given `offset` into `buf`.
     ///
     /// The output of the future is an `std::io::Result`, with `Ok(usize)` indicating the number of bytes read into `buf`.
@@ -41,16 +43,5 @@ pub trait Ops {
         file: OwnedFd,
         offset: u64,
         buf: B,
-    ) -> Pin<
-        Box<
-            dyn std::future::Future<
-                    Output = (
-                        (OwnedFd, B),
-                        Result<usize, crate::system::submission::op_fut::Error<std::io::Error>>,
-                    ),
-                >
-                + 'static
-                + Send,
-        >,
-    >;
+    ) -> BoxedOpFuture<crate::ops::read::ReadOp<B>>;
 }
