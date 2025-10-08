3e4752a41360ae2896f03af3b8305200fb536abf
Heikki Linnakangas <heikki@neon.tech>
2025-10-08 11:40:50 +0200

    remove unused 'slot' arg

--- tokio-epoll-uring/src/system/lifecycle/handle.rs
+++ tokio-epoll-uring/src/system/lifecycle/handle.rs
@@ -114,7 +114,6 @@ impl<M: PerSystemMetrics> crate::SystemHandle<M> {
         execute_op(
             op,
             inner.submit_side.weak(),
-            None,
             Arc::clone(&inner.per_system_metrics),
         )
     }
@@ -134,7 +133,6 @@ impl<M: PerSystemMetrics> crate::SystemHandle<M> {
         execute_op(
             op,
             inner.submit_side.weak(),
-            None,
             Arc::clone(&inner.per_system_metrics),
         )
     }
@@ -157,7 +155,7 @@ impl<M: PerSystemMetrics> crate::SystemHandle<M> {
         let per_system_metrics = Arc::clone(&inner.per_system_metrics);
         let weak = inner.submit_side.weak();
         futures::future::Either::Right(async move {
-            let (_, res) = execute_op(op, weak, None, per_system_metrics).await;
+            let (_, res) = execute_op(op, weak, per_system_metrics).await;
             res
         })
     }
@@ -177,7 +175,6 @@ impl<M: PerSystemMetrics> crate::SystemHandle<M> {
         execute_op(
             op,
             inner.submit_side.weak(),
-            None,
             Arc::clone(&inner.per_system_metrics),
         )
         .await
@@ -198,7 +195,6 @@ impl<M: PerSystemMetrics> crate::SystemHandle<M> {
         execute_op(
             op,
             inner.submit_side.weak(),
-            None,
             Arc::clone(&inner.per_system_metrics),
         )
         .await
@@ -225,7 +221,6 @@ impl<M: PerSystemMetrics> crate::SystemHandle<M> {
         let (resources, result) = execute_op(
             op,
             inner.submit_side.weak(),
-            None,
             Arc::clone(&inner.per_system_metrics),
         )
         .await;
@@ -265,7 +260,6 @@ impl<M: PerSystemMetrics> crate::SystemHandle<M> {
         execute_op(
             op,
             inner.submit_side.weak(),
-            None,
             Arc::clone(&inner.per_system_metrics),
         )
     }
--- tokio-epoll-uring/src/system/submission/op_fut.rs
+++ tokio-epoll-uring/src/system/submission/op_fut.rs
@@ -18,7 +18,7 @@ use crate::{
     metrics::PerSystemMetrics,
     system::{
         completion::ProcessCompletionsCause,
-        slots::{self, SlotHandle},
+        slots::self,
     },
 };
 
@@ -54,7 +54,6 @@ impl<T: Display> Display for Error<T> {
 pub(crate) async fn execute_op<O, M>(
     mut op: O,
     submit_side: SubmitSideWeak,
-    slot: Option<SlotHandle>,
     per_system_metrics: Arc<M>,
 ) -> (O::Resources, Result<O::Success, Error<O::Error>>)
 where
@@ -107,50 +106,46 @@ where
         }
     }
 
-    let mut slot = if let Some(slot) = slot {
-        slot
-    } else {
-        match open_guard.slots.try_get_slot() {
-            slots::TryGetSlotResult::Draining => {
-                return (
-                    op.on_failed_submission(),
-                    Err(Error::System(SystemError::SystemShuttingDown)),
-                )
-            }
-            slots::TryGetSlotResult::GotSlot { slot, queue_depth } => {
-                per_system_metrics
-                    .as_ref()
-                    .observe_slots_submission_queue_depth(queue_depth);
-                slot
-            }
-            slots::TryGetSlotResult::NoSlots { later, queue_depth } => {
-                // All slots are taken and we're waiting in line.
-                // If enabled, do some opportunistic completion processing to wake up futures that will release ops slots.
-                // This is in the hope that we'll wake ourselves up.
+    let mut slot = match open_guard.slots.try_get_slot() {
+        slots::TryGetSlotResult::Draining => {
+            return (
+                op.on_failed_submission(),
+                Err(Error::System(SystemError::SystemShuttingDown)),
+            )
+        }
+        slots::TryGetSlotResult::GotSlot { slot, queue_depth } => {
+            per_system_metrics
+                .as_ref()
+                .observe_slots_submission_queue_depth(queue_depth);
+            slot
+        }
+        slots::TryGetSlotResult::NoSlots { later, queue_depth } => {
+            // All slots are taken and we're waiting in line.
+            // If enabled, do some opportunistic completion processing to wake up futures that will release ops slots.
+            // This is in the hope that we'll wake ourselves up.
 
-                per_system_metrics
-                    .as_ref()
-                    .observe_slots_submission_queue_depth(queue_depth);
-                if *crate::env_tunables::PROCESS_COMPLETIONS_ON_QUEUE_FULL {
-                    // TODO shouldn't we loop here until we've got a slot? This one-off poll doesn't make much sense.
-                    open_guard.submitter.submit().unwrap();
-                    open_guard
-                        .completion_side
-                        .lock()
-                        .unwrap()
-                        .process_completions(ProcessCompletionsCause::Regular);
-                }
-                let slot = match later.await {
-                    Ok(slot) => slot,
-                    Err(_dropped) => {
-                        return (
-                            op.on_failed_submission(),
-                            Err(Error::System(SystemError::SystemShuttingDown)),
-                        )
-                    }
-                };
-                slot
+            per_system_metrics
+                .as_ref()
+                .observe_slots_submission_queue_depth(queue_depth);
+            if *crate::env_tunables::PROCESS_COMPLETIONS_ON_QUEUE_FULL {
+                // TODO shouldn't we loop here until we've got a slot? This one-off poll doesn't make much sense.
+                open_guard.submitter.submit().unwrap();
+                open_guard
+                    .completion_side
+                    .lock()
+                    .unwrap()
+                    .process_completions(ProcessCompletionsCause::Regular);
             }
+            let slot = match later.await {
+                Ok(slot) => slot,
+                Err(_dropped) => {
+                    return (
+                        op.on_failed_submission(),
+                        Err(Error::System(SystemError::SystemShuttingDown)),
+                    )
+                }
+            };
+            slot
         }
     };
 
